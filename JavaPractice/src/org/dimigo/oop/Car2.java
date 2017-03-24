2f37442aa10d45fda6637ee8d03fde9d64b628fd
홍승민 <@DESKTOP-QCMKPFN>
2017-03-24 15:30:48 +0900

    실습과제 4-2 제출

---
+++ JavaPractice/src/org/dimigo/oop/Car2.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *      |_  Car
+ * 
+ * 1. 개요:
+ * 2. 작성일 : 2017. 3. 23.
+ * </pre>
+ * 
+ * @author : 홍승민
+ * @version : 1.0
+ */
+
+public class Car2 {
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private int price;
+	
+	public Car2(){}
+	public Car2(String newCompany, String newModel, String newColor, int newMaxSpeed, int newPrice){
+		company = newCompany;
+		model = newModel;
+		color = newColor;
+		maxSpeed = newMaxSpeed;
+		price = newPrice;
+	}
+	
+	public String getCompany(){
+		return company;
+	}
+	public String getModel(){
+		return model;
+	}
+	public String getColor(){
+		return color;
+	}
+	public int getMaxSpeed(){
+		return maxSpeed;
+	}
+	public int getPrice(){
+		return price;
+	}
+
+	public void setCompany(String newcompany){
+		company= newcompany;
+	}
+	public void setModel(String newmodel){
+		model=newmodel;
+	}
+	public void setColor(String newcolor){
+		color = newcolor;
+	}
+	public void setMaxSpeed(int newmaxSpeed){
+		maxSpeed=newmaxSpeed;
+	}
+	public void setPrice(int newprice){
+		price = newprice;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ JavaPractice/src/org/dimigo/oop/CarTest2.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *      |_  CarTest
+ * 
+ * 1. 개요:
+ * 2. 작성일 : 2017. 3. 23.
+ * </pre>
+ * 
+ * @author : 홍승민
+ * @version : 1.0
+ */
+
+public class CarTest2 {
+	public static void main(String[] args) {
+		System.out.println("<< 자동차 목록 >>");
+
+		Car2 car = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
+
+		System.out.println("제조사명 : " + car.getCompany());
+		System.out.println("모델명 : " + car.getModel());
+		System.out.println("색상 : " + car.getColor());
+		System.out.println("최대속도 : " + car.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d", car.getPrice()) + "원");
+		System.out.println();
+
+		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+
+		System.out.println("제조사명 : " + car2.getCompany());
+		System.out.println("모델명 : " + car2.getModel());
+		System.out.println("색상 : " + car2.getColor());
+		System.out.println("최대속도 : " + car2.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d", car2.getPrice()) + "원");
+		System.out.println();
+
+		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+		
+		System.out.println("제조사명 : " + car3.getCompany());
+		System.out.println("모델명 : " + car3.getModel());
+		System.out.println("색상 : " + car3.getColor());
+		System.out.println("최대속도 : " + car3.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d", car3.getPrice()) + "원");
+	}
+}
